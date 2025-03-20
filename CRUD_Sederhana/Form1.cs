6540474be2e8f3fade1e84ad475a75a11d10311c
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-03-20 14:16:26 +0700

    membuat tabel untuk menampung data hasil query

--- CRUD_Sederhana/Form1.cs
+++ CRUD_Sederhana/Form1.cs
@@ -46,4 +46,5 @@ namespace CRUD_Sederhana
                     conn.Open();
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
                     
